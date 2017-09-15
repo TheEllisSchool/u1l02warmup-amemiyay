de1fbcdee74dae7e7e93d2d6da7fcb72d735fe3f
yumikaamemiya <yumikaamemiya@172.16.21.132>
2017-09-15 12:06:20 -0400

    converts to nickels and pennies

--- src/ChangeMaking.java
+++ src/ChangeMaking.java
@@ -6,7 +6,16 @@ public class ChangeMaking {
 		String input = JOptionPane.showInputDialog(null, "Enter an amount. "
 				+ "Use dollars and cents. \nExample: 1.56");
 		double num = Double.parseDouble(input);
-		JOptionPane.showMessageDialog(null, "You entered " + num);
+		//JOptionPane.showMessageDialog(null, "You entered " + num);
+		
+		int cents = (int) (num * 100);
+		int nickels = cents/5;
+		int pennies = cents - nickels * 5;
+		
+		JOptionPane.showMessageDialog(null, "$" + num + " is " 
+				+ nickels + " nickels and " 
+				+ pennies + " pennies.");
+		
 		
 		
 		
